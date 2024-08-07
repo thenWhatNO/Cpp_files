e1cac9687e1e6b67526ec89c6dc8a8c51424daaa
thenWhatNO <alexpley1889@gmail.com>
2024-08-07 20:18:47 +0300

    add func

--- test0.cpp
+++ test0.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
+using namespace std;
+
+void func();
+
 int main(){
 	std::cout  << "hello world" << std::endl;
 	return 0;
+}
+
+void func(){
+	int i;
+	for(int a = 0; a < 10; a++){
+		cout << a << endl;	
+	}
 }
