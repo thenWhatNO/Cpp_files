ec23a319ed2e7544e982d56be7d35db7521d7d45
thenWhatNO <alexpley1889@gmail.com>
2024-08-09 02:19:23 +0300

    try do snake game

--- test0.cpp
+++ test0.cpp
@@ -2,16 +2,46 @@
 
 using namespace std;
 
-void func();
+
+char Set_buffer(int x_, int y_);
+void render(int x_, int y_);
+int set_Snake();
 
 int main(){
-	std::cout  << "hello world" << std::endl;
+	int snakey[300][300] = set_snake();
+	char buffer = Set_buffer(100, 30, snakey);
+	render(100, 30, buffer);
 	return 0;
 }
 
-void func(){
-	int i;
-	for(int a = 0; a < 10; a++){
-		cout << a << endl;	
+int set_Snake(){
+	int snake_body[300][300];
+	
+	int c = 3;
+	for(int i = 0; i < 3; ++i){
+		snake_body[0][i] = c;
+		--c;		
 	}
+	return snake_body;
 }
+
+char Set_buffer(int x_, int y_, int snake[300][300]){
+	char C_name[y_][x_];
+
+	for(int l = 0; l < y_; ++l){
+		for (int r = 0; r < x_; ++r){
+			C_name[l][r] = '#';
+		}
+	}
+	return C_name;
+}
+
+void render(int x_, int y_, char C_name[100][30]){
+	for (int row = 0; row < y_; ++row){
+		for(int col = 0; col < x_; ++col){
+			cout << C_name[row][col];
+		}	
+		cout << '\n';
+	}	
+}
+
