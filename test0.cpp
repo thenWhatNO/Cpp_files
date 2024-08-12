b72e7b7e4666854244c7e11037d8e0ee602c595c
thenWhatNO <alexpley1889@gmail.com>
2024-08-12 21:05:43 +0300

    fix functio and add a new one

--- test0.cpp
+++ test0.cpp
@@ -1,42 +1,116 @@
 #include <iostream>
+#include <termios.h>
+#include <unistd.h>
+#include <sstream>
 
 using namespace std;
 
-
-char** Set_buffer(int x_, int y_, int** snake);
+char** Set_buffer(int x_, int y_, string* snake);
 void render(int x_, int y_, char** C_name);
-int** set_Snake();
+string* set_Snake();
+void updata_snake(string* snake, char last_move);
+char get_c();
 
 int main(){
-	int** snakey = set_Snake();
+	string* snakey = set_Snake();
 	char** buffer = Set_buffer(100, 30, snakey);
 	render(100, 30, buffer);
+	
+	//delete datas
+
+	for(int a = 0; a < 30; ++a){
+		delete[] buffer;
+	}
+	delete[] buffer;
+
+	delete[] snakey;
+
 	return 0;
 }
 
-int** set_Snake(){
-	int** snake_body = new int*[300];
-	for(int a = 0; a < 300; ++a){
-		snake_body[a] = new int[300];
+char get_c(){
+	struct termios oldt, newt;
+	char ch;
+
+	if(tcgetattr(STDIN_FILENO, &oldt) != 0){
+		perror("tcgtattr");
+		return -1;
 	}
-	
-	int c = 3;
-	for(int i = 0; i < 3; ++i){
-		snake_body[0][i] = c;
-		--c;		
+
+	newt = oldt;
+	newt.c_lflag &= ~(ICANON | ECHO);
+
+	if(tcsetattr(STDIN_FILENO, TCSANOW, &newt) != 0){
+		perror("tcsetattr");
+		return -1;
 	}
+
+	if(tcsetattr(STDIN_FILENO, TCSANOW, &oldt) != 0){
+		perror("tcsetattr");
+		return -1;
+	}
+
+	return ch;
+}
+
+string* set_Snake(){
+	string* snake_body = new string[300];
+
+	for (int i = 0; i < 300; ++i){
+		snake_body[i] = "";
+	}
+
+	snake_body[0] = "0, 3";
+
 	return snake_body;
 }
 
-char** Set_buffer(int x_, int y_, int** snake){
+string* updata_snake(string* snake, char last_move){
+
+	tringstream ss(snake[snake_point]);
+	int S_x, S_y;
+	char comma;
+	ss >> S_y >> comma >> S_x;
+
+	switch(last_move){
+		case 'w':
+			--S_y;
+			break;
+		case 'd':
+			++S_x;
+			break;
+		case 's':
+			++S_y;
+			break;
+		case 'a':
+			--S_x;
+			break;
+	}	
+	
+
+}
+
+char** Set_buffer(int x_, int y_, string* snake){
 	char** C_name = new char*[y_];
 	for(int i = 0; i < y_; ++i){
 		C_name[i] = new char[x_];
 	}
 
+	int snake_point = 0;
 	for(int l = 0; l < y_; ++l){
 		for (int r = 0; r < x_; ++r){
-			C_name[l][r] = '#';
+			stringstream ss(snake[snake_point]);
+			int S_x, S_y;
+			char comma;
+			ss >> S_y >> comma >> S_x;
+			if(S_y == l && S_x == r){
+				cout << snake[snake_point] << endl;
+				C_name[l][r] = 's';
+				++snake_point;
+			}
+			else {
+				C_name[l][r] = '#';
+			}
 		}
 	}
 	return C_name;
