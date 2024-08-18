a1a5d295cb7cfc0d1b1ed4df2508fdc4ce8ee1eb
thenWhatNO <alexpley1889@gmail.com>
2024-08-18 15:32:47 +0300

    updata the cod

--- test0.cpp
+++ test0.cpp
@@ -2,19 +2,23 @@
 #include <termios.h>
 #include <unistd.h>
 #include <sstream>
+#include <string>
 
 using namespace std;
 
 char** Set_buffer(int x_, int y_, string* snake);
 void render(int x_, int y_, char** C_name);
 string* set_Snake();
-void updata_snake(string* snake, char last_move);
+string* updata_snake(string* snake, char last_move, int snakee_langh);
 char get_c();
+bool snake_in(int Y, int X, int langh);
 
 int main(){
 	string* snakey = set_Snake();
 	char** buffer = Set_buffer(100, 30, snakey);
 	render(100, 30, buffer);
+	updata_snake(snakey, 'd', 1);
+	render(100, 30, buffer);	
 	
 	//delete datas
 
@@ -61,33 +65,68 @@ string* set_Snake(){
 	}
 
 	snake_body[0] = "0, 3";
+	snake_body[1] = "0, 2";
 
 	return snake_body;
 }
 
-string* updata_snake(string* snake, char last_move){
+string* updata_snake(string* snake, char last_move, int snakee_langh){
 
-	tringstream ss(snake[snake_point]);
-	int S_x, S_y;
-	char comma;
-	ss >> S_y >> comma >> S_x;
+	int snake_point = 0;
+	int prev_stap[2] = {0};
+	int new_step[2] = {0};
+	for (int i = 0; i < snakee_langh; ++i){
+		stringstream ss(snake[i]);
+		int S_x, S_y;
+		char comma;
+		ss >> S_y >> comma >> S_x;
+		prev_stap[0] = S_y; prev_stap[1] = S_x;
 
-	switch(last_move){
-		case 'w':
-			--S_y;
-			break;
-		case 'd':
-			++S_x;
-			break;
-		case 's':
-			++S_y;
-			break;
-		case 'a':
-			--S_x;
-			break;
-	}	
+		cout << prev_stap[0] << prev_stap[1] << endl;
+
+		if (snake_point == 0){
+			switch(last_move){
+				case 'w':
+					new_step[1] = S_x;
+					new_step[0] = --S_y;
+					break;
+				case 'd':
+					new_step[0] = S_y;
+					new_step[1] = ++S_x;
+					break;
+				case 's':
+					new_step[1] = S_x;
+					new_step[0] = ++S_y;
+					break;
+				case 'a':
+					new_step[0] = S_y;
+					new_step[1] = --S_x;
+					break;
+			}
+			cout << new_step[0] << new_step[1] << endl;
+		}
+		else{
+
+		}
+		snake[i] = to_string(new_step[0]) + ", " + to_string(new_step[1]);
+		new_step[0] = prev_stap[0]; new_step[1] = prev_stap[1];
+	}
 	
+	return snake;
+}
 
+bool snake_in(int Y, int X, int langh, string* snake){
+	for(int i = 0; i < langh; ++i){
+		stringstream ss(snake[i]);
+		int S_x, S_y;
+		char comma;
+		ss >> S_y >> comma >> S_x;
+
+		if (Y == S_y && X == S_x){
+			return true;
+		}
+	}
+	return false;
 }
 
 char** Set_buffer(int x_, int y_, string* snake){
@@ -96,17 +135,10 @@ char** Set_buffer(int x_, int y_, string* snake){
 		C_name[i] = new char[x_];
 	}
 
-	int snake_point = 0;
 	for(int l = 0; l < y_; ++l){
 		for (int r = 0; r < x_; ++r){
-			stringstream ss(snake[snake_point]);
-			int S_x, S_y;
-			char comma;
-			ss >> S_y >> comma >> S_x;
-			if(S_y == l && S_x == r){
-				cout << snake[snake_point] << endl;
+			if(snake_in(l, r, 2, snake)){
 				C_name[l][r] = 's';
-				++snake_point;
 			}
 			else {
 				C_name[l][r] = '#';
